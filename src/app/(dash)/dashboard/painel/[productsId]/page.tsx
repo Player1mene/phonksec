71eb971d382dc75a5dc18f6e576d8d514f03298b
gabriel <pleyerum@gmail.com>
2024-03-06 10:23:56 -0300

    dashboard/descontPage

--- src/app/(dash)/dashboard/painel/[productsId]/page.tsx
+++ src/app/(dash)/dashboard/painel/[productsId]/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import EditProducts from "../components/EditProducts";
+
+
+export const metadata: Metadata = {
+    title: 'Editar Produto - Painel',
+  }  
+  
 
 export default function page({ params }: { params: { productsId: string } }){
 
