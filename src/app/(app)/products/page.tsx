7218a8b85a545d3909ddd35306db264370cae40f
gabriel <pleyerum@gmail.com>
2024-03-08 16:31:23 -0300

    references

--- src/app/(app)/products/page.tsx
+++ src/app/(app)/products/page.tsx
@@ -1,5 +1,5 @@
-import ListProducts from '../..//components/ListProducts/ListProducts'
-import FilterProducts from '../../components/ListProducts/filterProducts/FilterProducts'
+import ListProducts from '../../components/ListProducts/ListProducts'
+import FilterProducts from '../../components/filterProducts/FilterProducts'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import { Metadata } from 'next'
