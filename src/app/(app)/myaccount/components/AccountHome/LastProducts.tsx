066ca8d6c1b6b0d60a55d60a6549c22686462a91
gabriel <pleyerum@gmail.com>
2024-03-13 19:29:53 -0300

    [update] Acentos nos textos

--- src/app/(app)/myaccount/components/AccountHome/LastProducts.tsx
+++ src/app/(app)/myaccount/components/AccountHome/LastProducts.tsx
@@ -8,21 +8,21 @@ export default function LastProducts(){
             <h3>Olá 👋, Usuário</h3>
             <div className={styles.lastGrid}>
                 <div className={styles.lastWishe}>
-                    <h4>Ultimo na lista de Desejos</h4>
+                    <h4>Último na lista de Desejos</h4>
                     <div className={styles.innerLast}>
 
                     </div>
                 </div>
 
                 <div className={styles.lastRequest}>
-                    <h4>Ultimo nos pediddos</h4>
+                    <h4>Último nos pedidos</h4>
                     <div className={styles.innerLast}>
                         
                     </div>
                 </div>
 
                 <div className={styles.lastCart}>
-                    <h4>Ultimo no carrinho</h4>
+                    <h4>Último no carrinho</h4>
                     <div className={styles.innerLast}>
                             
                     </div>
