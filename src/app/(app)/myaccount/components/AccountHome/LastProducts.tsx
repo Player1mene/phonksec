a4ebc92066bca86cf90c4925fb94d1a6df00d127
gabriel <pleyerum@gmail.com>
2024-03-14 10:56:59 -0300

    [change] usando useContext para pegar infos do usuario

--- src/app/(app)/myaccount/components/AccountHome/LastProducts.tsx
+++ src/app/(app)/myaccount/components/AccountHome/LastProducts.tsx
@@ -1,16 +1,21 @@
+'use client'
+import { AdminContext } from '@/app/adminContext';
 import styles from './LastProducts.module.css';
+import { useContext } from 'react';
 
 
 export default function LastProducts(){
 
+    const user = useContext(AdminContext);
+
     return (
         <div className={styles.LastProducts}>
-            <h3>Olá 👋, Usuário</h3>
+            <h3>Olá 👋, {user.user.name}</h3>
             <div className={styles.lastGrid}>
                 <div className={styles.lastWishe}>
                     <h4>Último na lista de Desejos</h4>
                     <div className={styles.innerLast}>
-
+                        
                     </div>
                 </div>
 
