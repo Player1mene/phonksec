b4dfea793e36f42842a50c97e6cd442ecb843a45
gabriel <pleyerum@gmail.com>
2024-03-15 22:50:59 -0300

    [change] tamanho das fontes do header e do footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -17,11 +17,14 @@ export default function Footer(){
             <div className="container">
                 <div className={styles.footerWrapper}>
                     <div className={styles.footerLogo}>
-                        <img src='https://cdn.discordapp.com/attachments/1091340777197670460/1213451432628453447/image.png?ex=65f585b6&is=65e310b6&hm=a34062f50e93aa2088a92e70b6d8d655cb142d9e81d68005cde6c2a1da8fa15a&' />
                         <button className={styles.goUp} onClick={handlerScrollTop}>Voltar para cima</button>
                     </div>
                     <div className={styles.footerNav}>
+                        <img src='https://cdn.discordapp.com/attachments/1091340777197670460/1213451432628453447/image.png?ex=65f585b6&is=65e310b6&hm=a34062f50e93aa2088a92e70b6d8d655cb142d9e81d68005cde6c2a1da8fa15a&' />
+                       
                         <ul>
+                            <li><h4>Navegação</h4></li>
+
                             <li><Link href="/">Home</Link></li>
 
                             <li><Link href="/products">Produtos</Link></li>
@@ -31,6 +34,8 @@ export default function Footer(){
                             <li><Link href="/contact">Contato</Link></li>
                         </ul>
                         <ul>
+                            
+                            <li><h4>Links de usuário</h4></li>
                             <li><Link href="/">Minha Conta</Link></li>
                             
                             <li><Link href="/">Carrinho</Link></li>
