25acb68b316627348209d29b0411c52011736d20
gabriel <pleyerum@gmail.com>
2024-03-15 22:39:50 -0300

    [change] tamanho das fontes do header e do footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer(){
                 <div className={styles.footerWrapper}>
                     <div className={styles.footerLogo}>
                         <img src='https://cdn.discordapp.com/attachments/1091340777197670460/1213451432628453447/image.png?ex=65f585b6&is=65e310b6&hm=a34062f50e93aa2088a92e70b6d8d655cb142d9e81d68005cde6c2a1da8fa15a&' />
-                        <button className={styles.goUp} onClick={handlerScrollTop}>↑ Voltar para cima</button>
+                        <button className={styles.goUp} onClick={handlerScrollTop}>Voltar para cima</button>
                     </div>
                     <div className={styles.footerNav}>
                         <ul>
@@ -45,7 +45,7 @@ export default function Footer(){
                 </div>
             </div>
             <div className={styles.copyright}>
-                        <p>COPYRIGHT © 2024 PHONKSEC TODOS OS DIREITOS RESERVADOS</p>
+                        <p>Copyright © 2024 PHONKSEC Todos os direitos reservados</p>
             </div>
         </div>
     )
