f5f047fc6c9de13826be0705e3b38855e250458e
gabriel <pleyerum@gmail.com>
2024-03-26 12:19:10 -0300

    [create] navegação responsiva na página de usuário

--- src/app/(app)/myaccount/components/AccountHeader/AccountHeader.tsx
+++ src/app/(app)/myaccount/components/AccountHeader/AccountHeader.tsx
@@ -1,20 +1,25 @@
+'use client'
 import styles from './AccountHeader.module.css'
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBagShopping, faHeart, faShoppingCart, faUser, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
+import { faBagShopping, faBars, faHeart, faShoppingCart, faUser, faXmark, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
 import { auth } from "@/app/db/firebase";
 import { signOut } from "firebase/auth";
 import { AdminContext } from '@/app/adminContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export default function AccountHeader(){
 
     const {logOut} = useContext(AdminContext)
+    const [menu, setMenu] = useState<boolean>(false)
 
     return (
-    <div className={styles.header}>        
+    <div className={styles.header}>   
 
-        <h3>Minha Conta</h3>
+        <button className={styles.menu} onClick={()=>{setMenu(!menu)}}>{menu ? <FontAwesomeIcon icon={faXmark}/> : <FontAwesomeIcon icon={faBars}/>}</button>
+
+        <div className={`${styles.headerWrapper} ${!menu && styles.headerFadeOut}`} >
+                <h3>Minha Conta</h3>
 
         <ul>
             <li><Link href="/myaccount"><FontAwesomeIcon icon={faUser} /> Visão geral</Link></li>
@@ -22,13 +27,15 @@ export default function AccountHeader(){
             <li><Link href="/myaccount"><FontAwesomeIcon icon={faHeart} /> Lista de Desejos</Link></li>
             
             <li><Link href="/myaccount"><FontAwesomeIcon icon={faBagShopping}/> Meus pedidos</Link></li>
-        
+
             <li><Link href="/myaccount"><FontAwesomeIcon icon={faShoppingCart}/> Carrinho</Link></li>
 
             <li><Link href="#" onClick={()=>{logOut()}}><FontAwesomeIcon icon={faXmarkCircle}/> Fazer logout</Link></li>
         </ul>
 
-    </div>
+
+        </div>
+           </div>
     
     )
 }
